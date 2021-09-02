2829787d66c54a5bb497098cc22fe19b417b899e
shawn barbel <barbels33@gmail.com>
2021-09-02 16:21:13 -0400

    display categories, passed data to gridTile comp

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -10,23 +10,21 @@ import {
 
 import { CATEGORIES } from "../dummy-data";
 import Colors from "../constants/Colors";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() => {
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate("CategoryMeals", {
             categoryId: itemData.item.id,
             categoryTitle: itemData.item.title,
           });
         }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      ></CategoryGridTile>
     );
   };
   return (
@@ -58,11 +56,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
   },
 });
 
