1487fbba1887681e8664011ddadf0502e9e652cd
shawn barbel <barbels33@gmail.com>
2021-09-02 19:27:16 -0400

    passed data to meal details

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import Colors from "../constants/Colors";
-import { CATEGORIES } from "../dummy-data";
+
 import { MEALS } from "../dummy-data (1)";
 import MealItem from "../components/MealItem";
 
 const CategoryMealsScreen = ({ navigation, route }) => {
-  const { categoryTitle, categoryId } = route.params;
+  const { categoryId } = route.params;
 
   const displayMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(categoryId) >= 0
@@ -16,11 +16,16 @@ const CategoryMealsScreen = ({ navigation, route }) => {
     return (
       <MealItem
         title={itemData.item.title}
-        onSelectMeal={() => {}}
         duration={itemData.item.duration}
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
         image={itemData.item.imageUrl}
+        onSelectMeal={() => {
+          navigation.navigate("MealDetails", {
+            mealId: itemData.item.id,
+            mealTitle: itemData.item.title,
+          });
+        }}
       ></MealItem>
     );
   };
