a0ef5a2d06e99422c420efea8f3cb2bc965406db
shawn barbel <barbels33@gmail.com>
2021-09-03 11:56:21 -0400

    created fav navigator for favList and meaDetails

---
+++ navigation/FavoriteNavigator.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from "@react-navigation/stack";
+import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from "../screens/FavoriteScreen";
+
+import Colors from "../constants/Colors";
+
+const Stack = createStackNavigator();
+
+const FavoriteNavigator = () => {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator
+        screenOptions={{
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          headerStyle: {
+            backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+          },
+          headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+        }}
+      >
+        <Stack.Screen
+          name="Favorites"
+          component={FavoritesScreen}
+          options={{
+            title: "Your Favorites",
+          }}
+        />
+        <Stack.Screen
+          options={({ route }) => ({ title: route.params.mealTitle })}
+          name="MealDetails"
+          component={MealDetailScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export { FavoriteNavigator };
--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import MealList from "../components/MealList";
+import { MEALS } from "../dummy-data (1)";
 
-const FavoriteScreen = () => {
+const FavoriteScreen = (props) => {
+  const favMeals = MEALS.filter((meal) => meal.id === "m1" || meal.id === "m2");
   return (
-    <View style={styles.screen}>
-      <Text>The Favorite Screen</Text>
-    </View>
+    <MealList listData={favMeals} navigation={props.navigation}></MealList>
   );
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default FavoriteScreen;
