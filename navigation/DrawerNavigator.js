a425451cbc6491398b3984afe02036e13222945a
shawn barbel <barbels33@gmail.com>
2021-09-04 22:19:32 -0400

    created drawer navigator, imported tabNavigator

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import FiltersScreen from "../screens/FiltersScreen";
+import TabNavigator from "./TabNavigator";
+import Colors from "../constants/Colors";
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator initialRouteName="CategoriesScreen">
+      <Drawer.Screen
+        name="Meal Categories"
+        component={TabNavigator}
+        options={{
+          headerShown: false,
+          title: "Categories",
+          drawerActiveTintColor: Colors.secondary,
+          fontFamily: "open-sans-bold",
+        }}
+      ></Drawer.Screen>
+      <Drawer.Screen
+        name="Filters"
+        component={FiltersScreen}
+        options={{
+          titile: "Filters",
+          headerShown: false,
+          drawerActiveTintColor: Colors.secondary,
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerNavigator;
