24bb701d6f8bc07959e52b6e3a20aac16f7d537f
shawn barbel <barbels33@gmail.com>
2021-09-09 22:28:21 -0400

    removed unused code

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { View, StyleSheet, FlatList } from "react-native";
 
 import Colors from "../constants/Colors";
 import MealList from "../components/MealList";
