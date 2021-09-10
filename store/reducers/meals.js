97dd4b97f83e1b6eb60e92d8594202a1935a6cbe
shawn barbel <barbels33@gmail.com>
2021-09-09 21:43:11 -0400

    created toggle favoritre action

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,4 +1,5 @@
 import { MEALS } from "../../mealsData";
+import { toggleFavorite, TOGGLE_FAVORITE } from "../actions/meals";
 
 const initialState = {
   meals: MEALS,
@@ -7,7 +8,29 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case toggleFavorite:
+      ///does product exist
+      const existingIndex = state.favoriteMeals.find(
+        (meal) => meal.id === action.mealId
+      );
+
+      ///meal exist, remove from fav array
+      if (existingIndex >= 0) {
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return { ...state, favoriteMeals: updatedFavMeals };
+      } else {
+        /// does not exist, add to fav meal list
+        const meal = state.meals.find((meal) => meal.id === action.mealId);
+        return {
+          ...state,
+          favoriteMeals: state.favoriteMeals.concat(meal),
+        };
+      }
+    default:
+      return state;
+  }
 };
 
 export default mealsReducer;
